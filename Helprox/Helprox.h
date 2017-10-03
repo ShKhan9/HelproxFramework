2c2a6f40e94948c5ad84bdbca9d223b9a636ecda
shehata <shehatagamal120@yahoo.com>
2017-10-03 13:20:08 +0200

    Initial Commit

---
+++ Helprox/Helprox.h
@@ -0,0 +1,19 @@
+//
+//  Helprox.h
+//  Helprox
+//
+//  Created by ShKhan on 10/3/17.
+//  Copyright © 2017 Approcks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Helprox.
+FOUNDATION_EXPORT double HelproxVersionNumber;
+
+//! Project version string for Helprox.
+FOUNDATION_EXPORT const unsigned char HelproxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Helprox/PublicHeader.h>
+
+
